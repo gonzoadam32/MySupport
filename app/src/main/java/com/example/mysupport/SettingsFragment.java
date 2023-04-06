e33dfcde071880bf01edffd3782a8e6cae2cc63f
4015371 <cloutads728@gmail.com>
2023-04-06 12:48:07 -0500

    List preference not working.

--- app/src/main/java/com/example/mysupport/SettingsFragment.java
+++ app/src/main/java/com/example/mysupport/SettingsFragment.java
@@ -1,11 +1,15 @@
 package com.example.mysupport;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 
 import androidx.annotation.Nullable;
+import androidx.preference.ListPreference;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment{
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -14,6 +18,18 @@ public class SettingsFragment extends PreferenceFragment {
         // below line is used to add preference
         // fragment from our xml folder.
         addPreferencesFromResource(R.xml.preferences);
+
+
+        PreferenceScreen p = getPreferenceScreen();
+
+        ListPreference listPreference;
+
+        Preference t =  p.getPreference(0);
+        if(t instanceof android.preference.ListPreference) {
+            listPreference = (ListPreference) t;
+        }
+
+        //listPreference = (ListPreference) findPreference("@string/key_notifications");
     }
 }
 
